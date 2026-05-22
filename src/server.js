e32e51703347b4cec94a0c929cce2f540689ff1a
Yevheniia Uhnich <kjaine@gmail.com>
2026-05-22 11:18:40 +0300

    2 tasks

--- src/server.js
+++ src/server.js
@@ -1,19 +1,19 @@
+// src/server.js
+
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { getEnvVar } from './utils/getEnvVar.js';
-import { getAllStudents, getStudentById } from './services/students.js';
 
+import studentsRouter from './routers/students.js'; // Імпортуємо роутер
+import { getEnvVar } from './utils/getEnvVar.js';
 
-
-const PORT = Number(getEnvVar('PORT', '3001')) || 3001;
+const PORT = Number(getEnvVar('PORT', '3000'));
 
 export const startServer = () => {
   const app = express();
 
   app.use(express.json());
   app.use(cors());
- 
 
   app.use(
     pino({
@@ -24,52 +24,27 @@ export const startServer = () => {
   );
 
   app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world!',
-  });
-});
-
-  app.get('/students', async (req, res) => {
-     const students = await getAllStudents();
-
-         res.status(200).json({
-      data: students,
+    res.json({
+      message: 'Hello World!',
     });
   });
 
-   app.get('/students/:studentId', async (req, res) => {
-        const { studentId } = req.params;
-    const student = await getStudentById(studentId); 
+  app.use(studentsRouter); // Додаємо роутер до app як middleware
 
-    	if (!student) {
-	  res.status(404).json({
-		  message: 'Student not found'
-	  });
-	  return;
-	}
-
-        res.status(200).json({
-      data: student,
+  app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
     });
   });
 
-  app.use((req, res) => {
-  res.status(404).json({
-    message: 'Not found',
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
   });
-});
-
-  // eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-});
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
-
-  
 };
